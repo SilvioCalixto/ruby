869146bc853f459de2b5b380a13e381993f112cb
Silvio Calixto <silvio.calixto.sc@gmail.com>
2024-07-10 18:28:11 -0300

    atualizei

---
+++ rubynapratica/desafio5.rb
@@ -0,0 +1,6 @@
+print 'Digite sua altura: '
+altura = gets.chomp.to_f
+cen = altura * 100
+mili = altura * 1000
+
+puts " A altura digitada é #{altura}, a conversão em centimetros é #{cen}, em milimetros é #{mili}"
