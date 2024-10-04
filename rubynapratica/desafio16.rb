e414ecdfd8708edbfd14f08467610d5feeeb2fd3
Silvio Calixto <silvio.calixto.sc@gmail.com>
2024-10-04 19:34:36 -0300

    add

---
+++ rubynapratica/desafio16.rb
@@ -0,0 +1,10 @@
+# Crie um script em Ruby que leia uma distância (em Km) entre dois pontos e o preço da gasolina em reais.
+# Depois, calcule e mostre quantos litros de gasolina o carro irá consumir e quanto será o gasto em reais. Considere que o carro consegue percorrer 12 Km com um litro de gasolina.
+
+puts "Digite a distancia em Km: "
+distancia = gets.chomp.to_i
+
+puts 'Digite o preço da gasolina em R$: '
+preco = gets.chom.to_f
+
+desempenho = 
